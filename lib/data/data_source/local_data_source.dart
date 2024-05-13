fc04caec49b4887825b340e0e36ac7aecee6bc3a
ss <mahmoudmagdy101019@gmail.com>
2024-05-14 02:00:58 +0300

    create translations file

--- lib/data/data_source/local_data_source.dart
+++ lib/data/data_source/local_data_source.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: constant_identifier_names, prefer_collection_literals
+
 import 'package:tut_app/data/network/error_handler.dart';
 import 'package:tut_app/data/response/responses.dart';
 
